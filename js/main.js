ed5cd92f0a1ceefd1f0795d55ce02185f74dd48b
Ілля <Illastibel2005ua@gmail.com>
2023-01-25 19:43:23 +0200

    Test
    tesy

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function() {
+
+    $('.bike-slider, .slider__items').slick({
+    	arrows: false,
+    	dots: true,
+    	fade: true,
+    	autoplay: true,
+  		autoplaySpeed: 2000
+    });
+
+});
+
+/* fade:true включає ефект плавного переключення слайда */
