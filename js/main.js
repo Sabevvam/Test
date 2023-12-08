2ea5545e6f9d24dfbc1083fa6ba6a361c13b6103
Ilya_century <61396442+Sabevvam@users.noreply.github.com>
2023-12-08 11:45:59 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,13 +1,42 @@
-$(function() {
+const tabItem = document.querySelectorAll(".tabs__btn-item");
+const tabContent = document.querySelectorAll(".tabs__content-item");
 
-    $('.bike-slider, .slider__items').slick({
-    	arrows: false,
-    	dots: true,
-    	fade: true,
-    	autoplay: true,
-  		autoplaySpeed: 2000
-    });
-
+tabItem.forEach(function(element) {
+  element.addEventListener('click', open);
 });
 
-/* fade:true включає ефект плавного переключення слайда */
+function open(evt) {
+  const tabTarget = evt.currentTarget;
+  const button = tabTarget.dataset.button;
+
+  tabItem.forEach(function(item) {
+    item.classList.remove("tabs__btn-item--active");
+  });
+
+  tabTarget.classList.add("tabs__btn-item--active");
+
+  tabContent.forEach(function(item) {
+    item.classList.remove("tabs__content-item--active");
+  });
+
+  document.getElementById(button).classList.add("tabs__content-item--active");
+}
+
+const menuBtn = document.querySelector(".menu__btn");
+const menu = document.querySelector(".menu__list");
+
+menuBtn.addEventListener("click", () => {
+  menu.classList.toggle('menu__list--active'); // Заміни "actie" на "active"
+});
+
+var swiper = new Swiper(".swiper", {
+  effect: "fade",
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+});
+
